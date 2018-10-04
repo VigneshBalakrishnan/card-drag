9d9c137a5e5ad80360780461d735b9ea27b5011a
Vignesh BalaKrishnan <vbalakrishnan@corp.zuora.com>
2018-10-05 00:03:29 +0530

    added dragable functionality and move required changes

--- card-drag/src/app/app.component.ts
+++ card-drag/src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'card-drag';
+  data: any = [{ id: 'A', name: 'BV' }, { id: 'B', name: 'TT' }];
+  onDragStart(e: any, data: any) {
+    // SET DATATRANSFER OF DATA
+    // event["dataTransfer"].setData('data', JSON.stringify(data));
+    event['dataTransfer'].setData('data', JSON.stringify(data));
+    console.log(data);
+  }
+  click(a) {
+    this.data = this.data.filter(item => item.id !== a.id);
+    console.log(this.data, a.id);
+  }
+  allowDrop(e) {
+    e.preventDefault();
+  }
+  onDrop(e, a) {
+    e.preventDefault();
+    const data = e.dataTransfer.getData('data');
+    console.log(data, a);
+    this.data = this.data.map(
+      item => (item.id === a.id ? (item.childData = [JSON.parse(data)]) : item)
+    );
+  }
 }
